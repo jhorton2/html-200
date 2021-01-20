c962965bc890147712fdb0b4b54bac04f2dbca89
Josh Horton <joshhorton2244@gmail.com>
2021-01-19 20:06:43 -0800

    lesson3-assignment1

---
+++ html200/lesson03/Lesson03Challenge3.js
@@ -0,0 +1,5 @@
+let WAcities = ['0,Seattle', '1,Tacoma', '2,Bellevue', '3,Everett', '4,Vancouver','5,Spokane'];
+
+for (let i = 0; i < WAcities.length; i++) {
+  console.log(WAcities[i]);
+}
---
+++ html200/lesson03/exercise-one.js
@@ -0,0 +1,31 @@
+1.
+
+let hockeycountries = ['canada','usa','finland','russia','sweden'];
+
+console.log(hockeycountries);
+
+
+2.
+
+ let hockeycountries = ['canada','usa','finland','russia','sweden'];
+
+hockeycountries.push('switzerland')
+
+console.log(hockeycountries);
+
+
+3.
+
+let hockeycountries = ['canada','usa','finland','russia','sweden'];
+
+hockeycountries.splice(2, 1);
+
+console.log(hockeycountries);
+
+4.
+
+let hockeycountries = ['canada','usa','finland','russia','sweden'];
+
+const hockeycountrieslist = hockeycountries.join (', ');
+
+console.log(hockeycountrieslist);
